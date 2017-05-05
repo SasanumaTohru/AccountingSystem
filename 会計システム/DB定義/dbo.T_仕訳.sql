463745d51fe43794b3d8dffe5107def96ae58edf
笹沼 透 <sasanuma@kanagawa-jk.or.jp>
2017-05-05 12:07:51 +0900

    create table ファイルを追加

---
+++ 会計システム/DB定義/dbo.T_仕訳.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[T_仕訳] (
+    [伝票番号]    NCHAR (12)    NOT NULL,
+    [仕訳番号]    INT           NOT NULL,
+    [勘定科目コード] INT           NOT NULL,
+    [金額]      DECIMAL (18)  NOT NULL,
+    [摘要]      NVARCHAR (50) NOT NULL,
+    [貸借]      INT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([伝票番号] ASC, [仕訳番号] ASC)
+);
+
---
+++ 会計システム/DB定義/dbo.T_会計伝票.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[T_会計伝票] (
+    [伝票番号] NCHAR (12) NOT NULL,
+    [計上日]  DATE       NOT NULL,
+    PRIMARY KEY CLUSTERED ([伝票番号] ASC)
+);
+
