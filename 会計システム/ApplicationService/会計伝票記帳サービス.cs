80c29c085b638295056558dbc599fd6f1816f2a3
笹沼 透 <sasanuma@kanagawa-jk.or.jp>
2017-05-27 10:11:32 +0900

    ヌケを修正

--- 会計システム/ApplicationService/会計伝票記帳サービス.cs
+++ 会計システム/ApplicationService/会計伝票記帳サービス.cs
@@ -8,6 +8,10 @@ namespace AccountingSystem.ApplicationService
         public 伝票 伝票を記帳する(伝票 伝票)
         {
             var 伝票ファイリングサービス = new Infrastructure.会計伝票ファイリングサービス();
+            if (伝票.貸借金額不一致)
+            {
+                throw new System.Exception("貸借の金額が一致していません。");
+            }
             return 伝票ファイリングサービス.伝票を保存する(伝票);
         }
           
